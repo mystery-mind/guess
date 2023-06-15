acaa2731856031f99d98af40fe09d915fbe69db4
mystery-mind <123958834+mystery-mind@users.noreply.github.com>
2023-06-15 19:56:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// Create the login function here.
+function Login(){
+    player_name=document.getElementById("playername").value;
+    localStorage.setItem("player_name",player_name);
+    window.location="index1.html";
+}
